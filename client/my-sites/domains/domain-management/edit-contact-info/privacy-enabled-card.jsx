760d6dca4557cf8742687c96d41a7f209a184f37
Jarda Snajdr <jsnajdr@gmail.com>
2019-03-27 13:19:11 +0100

    Domain Contact Info: migrate CSS of whois/privacy notices to webpack

--- client/my-sites/domains/domain-management/edit-contact-info/privacy-enabled-card.jsx
+++ client/my-sites/domains/domain-management/edit-contact-info/privacy-enabled-card.jsx
@@ -3,10 +3,8 @@
 /**
  * External dependencies
  */
-
 import React from 'react';
-
-import { localize } from 'i18n-calypso';
+import { useTranslate } from 'i18n-calypso';
 
 /**
  * Internal dependencies
@@ -14,24 +12,27 @@ import { localize } from 'i18n-calypso';
 import Card from 'components/card';
 import { CALYPSO_CONTACT } from 'lib/url/support';
 
-class EditContactInfoPrivacyEnabledCard extends React.Component {
-	render() {
-		return (
-			<Card className="edit-contact-info-privacy-enabled-card">
-				<p className="edit-contact-info-privacy-enabled-card__settings-explanation">
-					{ this.props.translate(
-						'This domain is currently using Privacy Protection to keep your information from showing up in public record searches. ' +
-							"If you need to make a change to your domain's contact info, please {{a}}contact support{{/a}}.",
-						{
-							components: {
-								a: <a href={ CALYPSO_CONTACT } target="_blank" rel="noopener noreferrer" />,
-							},
-						}
-					) }
-				</p>
-			</Card>
-		);
-	}
-}
+/**
+ * Style dependencies
+ */
+import './privacy-enabled-card.scss';
 
-export default localize( EditContactInfoPrivacyEnabledCard );
+export default function EditContactInfoPrivacyEnabledCard() {
+	const translate = useTranslate();
+
+	return (
+		<Card className="edit-contact-info__privacy-enabled-card">
+			<p className="edit-contact-info__privacy-enabled-card-settings-explanation">
+				{ translate(
+					'This domain is currently using Privacy Protection to keep your information from showing up in public record searches. ' +
+						"If you need to make a change to your domain's contact info, please {{a}}contact support{{/a}}.",
+					{
+						components: {
+							a: <a href={ CALYPSO_CONTACT } target="_blank" rel="noopener noreferrer" />,
+						},
+					}
+				) }
+			</p>
+		</Card>
+	);
+}
